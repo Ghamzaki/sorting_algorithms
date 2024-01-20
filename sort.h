9c231c89ce9b477068341baa88aa199f401516dd
Ghamzaki <uthmanghamzaki@gmail.com>
2024-01-20 12:28:34 +0000

    First task

--- sort.h
+++ sort.h
@@ -1,47 +1,12 @@
+#ifndef SORT_H
+#define SORT_H
+
 #include <stdlib.h>
 #include <stdio.h>
 
-/**
- * print_array - Prints an array of integers
- *
- * @array: The array to be printed
- * @size: Number of elements in @array
- */
-void print_array(const int *array, size_t size)
-{
-    size_t i;
-
-    i = 0;
-    while (array && i < size)
-    {
-        if (i > 0)
-            printf(", ");
-        printf("%d", array[i]);
-        ++i;
-    }
-    printf("\n");
-}
-
-/**
- * print_list - Prints a list of integers
- *
- * @list: The list to be printed
- */
-void print_list(const listint_t *list)
-{
-    int i;
-
-    i = 0;
-    while (list)
-    {
-        if (i > 0)
-            printf(", ");
-        printf("%d", list->n);
-        ++i;
-        list = list->next;
-    }
-    printf("\n");
-}
+/* functions print_array and print_list */
+void print_array(const int *array, size_t size);
+void print_list(const listint_t *list);
 
 /**
  * struct listint_s - Doubly linked list node
@@ -55,4 +20,10 @@ typedef struct listint_s
     const int n;
     struct listint_s *prev;
     struct listint_s *next;
-} listint_t;
+} listint_t
+
+/* sorting algorithms */
+void bubble_sort(int *array, size_t size);
+
+
+#endif SORT_H
